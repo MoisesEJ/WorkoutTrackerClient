7990a28024d382b90af57f8467ad6c8e57383efb
MoisesEJ <moisesej.19@gmail.com>
2025-11-14 22:31:51 -0600

    correction of path "ThemeUser"

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ThemeProvider } from '@contexts/ThemeUser'
+import { ThemeProvider } from './contexts/ThemeUser'
 import '@styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
