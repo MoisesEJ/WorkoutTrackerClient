552d470de81ec7a3594c8396e4d5dfdab78be58b
MoisesEJ <moisesej.19@gmail.com>
2025-11-15 14:09:44 -0600

    modified version vite and node

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
 import path from 'path'
 
 // https://vite.dev/config/
@@ -8,10 +7,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: 'localhost',
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
-    },
     port: 5173
   },
   resolve: {
